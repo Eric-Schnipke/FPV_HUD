164a83d770793446c9a7a7760c8d9388ed2063c1
Eric-Schnipke <eric.l.schnipke@gmail.com>
2015-05-02 01:42:00 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,57 @@
+//
+//  main.cpp
+//  Blinking Light Tracker
+//
+//  Created by Eric Schnipke on 5/1/15.
+//  Copyright (c) 2015 Eric Schnipke. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include "Camera.h"
+
+/* Camera feed frame titles */
+const cv::String FRAME_TITLE1 = "BGR Frame";
+const cv::String FRAME_TITLE2 = "HSV Frame";
+const cv::String FRAME_TITLE3 = "Binary Frame";
+
+const unsigned short int CAMERA_ADDRESS = 0;
+const unsigned short int FRAME_DELAY = 1;
+
+int main(int argc, const char * argv[])
+{
+    /* camera object with which will track */
+    camera thisCamera;
+    
+    /* matrix in which to store our video frame */
+    cv::Mat frame;
+    
+    /* user control character */
+    char controlChar;   
+    
+    /* create a new camera object and initialize to default webcam address */
+    thisCamera.initialize(CAMERA_ADDRESS);
+    
+    /* enter main loop */
+    while(controlChar != 'q')
+    {
+        /* advance the camera one frame */
+        thisCamera.getFrame(frame);
+        thisCamera.blurFrame(frame);
+        thisCamera.showFrame(FRAME_TITLE1, frame);
+        thisCamera.convertFrameToHsv(frame);
+        thisCamera.showFrame(FRAME_TITLE2, frame);
+        thisCamera.convertHsvFrametoBinary(frame);
+        thisCamera.showFrame(FRAME_TITLE3, frame);
+        
+        /* capture program control character */
+        controlChar = cv::waitKey(FRAME_DELAY);
+    }
+    
+    /* return camera resources back to system */
+    thisCamera.destroy();
+    
+    return 0;
+}
+
